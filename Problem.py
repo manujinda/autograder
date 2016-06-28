c03e16d90316b56ecc7a121800aab8d95b33804c
Manujinda Wathugala <manu@cs.uoregon.edu>
2016-06-28 15:40:39 -0700

    Added some instance variables to class Problem

--- Problem.py
+++ Problem.py
@@ -3,4 +3,14 @@ class Problem(object):
         A class to represent a single problem or a Homework
     """
     
-    
+    def __init__(self, no):
+        self.prob_no = no
+        self.proj_desc = 'Problem description'
+        self.files_provided = []
+        self.files_submitted = []
+        self.inp_outps = {} # A dictionalry that maps test inputs to anticipated outputs
+        self.language = 'Programming language used'
+        self.com_lin_opt = False
+        self.stud_mak_fil = False
+        self.mak_targs = []
+        self.scores = []
