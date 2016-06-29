0f31349e999cd176d4047b9d53ec22c9ed2010f8
Manujinda Wathugala <manu@cs.uoregon.edu>
2016-06-29 16:44:47 -0700

    Configuration file reading added. Need to decide the fields of each configuration file

--- Problem.py
+++ Problem.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import csv
+
 class Problem(object):
     """
         A class to represent a single problem or a Homework
@@ -5,12 +9,33 @@ class Problem(object):
     
     def __init__(self, no):
         self.prob_no = no
+        self.name = ''
         self.proj_desc = 'Problem description'
         self.files_provided = []
         self.files_submitted = []
         self.inp_outps = {} # A dictionalry that maps test inputs to anticipated outputs
         self.language = 'Programming language used'
-        self.com_lin_opt = False
-        self.stud_mak_fil = False
-        self.mak_targs = []
+        self.command_line_options = False
+        self.student_make_file = False
+        self.make_targs = []
         self.scores = []
+
+    def __str__(self):
+        return '{} {}'.format(self.prob_no, self.name)
+        
+    def setup_problem(self, config_path):
+        # Check whether the problem configuration file exisits.
+        if not os.path.exists(config_path):
+            print '\nProblem configuration file {} does not exist, exit...'.format(config_path)
+            sys.exit()
+        
+        with open(config_path) as config_file:
+            reader = csv.DictReader(config_file)
+            for row in reader:
+                print row['Key'], row['Value']
+                if row['Key'] ==  'prob_no':
+                    self.prob_no = int(row['Value'])
+                elif row['Key'] == 'name':
+                    self.name = row['Value']
+            
+        
--- Project.py
+++ Project.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python 
 
 import datetime
+import os
+import sys
+import csv
 
-import Problem
+from Problem import Problem
 
 class Project(object):
     """
@@ -11,15 +14,78 @@ class Project(object):
     """
     def __init__(self):
         self.proj_no = 0
+        self.name = ''
         self.duedate = datetime.date(2016, 6, 28)
+        self.gradingroot = ''
+        self.masterdir = ''
         self.subdir = 'assignment'
-        self.problems = []
+        self.problem_ids = []
+        self.problems = {}
         
-        for i in range(3):
-            self.problems.append(Problem.Problem(i))
+        #for i in range(3):
+        #    self.problems.append(Problem.Problem(i))
+        #    
+        #print 'initialized'
+        #for i in range(3):
+        #    print self.problems[i].prob_no
             
-        print 'initialized'
-        for i in range(3):
-            print self.problems[i].prob_no
         
-p = Project()
+    def setup_project(self, config_path):
+        
+        # Check whether the file exists.
+        if not os.path.exists(config_path):
+            print '\nConfiguration file {} does not exist, exit...'.format(config_path)
+            sys.exit()
+
+        #config_dict = {}
+        with open(config_path) as config_file:
+            reader = csv.DictReader(config_file)
+            for row in reader:
+                print row['Key'], row['Value']
+                if row['Key'] ==  'proj_no':
+                    self.proj_no = int(row['Value'])
+                elif row['Key'] == 'name':
+                    self.name = row['Value']
+                elif row['Key'] == 'duedate':
+                    self.duedate = datetime.datetime.strptime(row['Value'], '%m/%d/%Y')
+                elif row['Key'] == 'gradingroot':
+                    self.gradingroot = row['Value']
+                elif row['Key'] == 'subdir':
+                    self.subdir = row['Value']
+                elif row['Key'] == 'problems':
+                    self.problem_ids = row['Value'].split()
+                #config_dict[row['Key']] = row['Value']
+                
+        for p in self.problem_ids:
+            prob_conf = os.path.join(self.gradingroot, 'assignments', self.subdir, 'prob_' + p + '.csv')
+            print prob_conf
+            
+            # Check whether the problem configuration file exisits.
+            if not os.path.exists(prob_conf):
+                print '\nProblem configuration file {} does not exist, exit...'.format(prob_conf)
+                sys.exit()
+            
+            self.problems[p] = Problem(p)
+            
+            self.problems[p].setup_problem(prob_conf)
+            
+            #continue
+
+            #prob_conf = 'C:\\Users\\manujinda\\Documents\\++grading\\assignments\\assignment1\\assignment1.csv'            
+            #with open(prob_conf) as config_file:
+            #    reader = csv.DictReader(config_file)
+            #    for row in reader:
+            #        print row['Key'], row['Value']
+            #        if row['Key'] ==  'prob_no':
+            #            self.problems[p].prob_no = int(row['Value'])
+            #        elif row['Key'] == 'name':
+            #            self.problems[p].name = row['Value']
+            #    
+            #    print self.problems[p]
+                
+        #print self.problems
+        for p in self.problems.keys():
+            print self.problems[p]
+        
+#p = Project()
+#p.test_meth()
