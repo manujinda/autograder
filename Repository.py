65972e3de8378a7639f6ad0633ecf1688fda2fa7
Manujinda Wathugala <manu@cs.uoregon.edu>
2016-07-11 18:10:13 -0700

    A class to encapsulate git stuff. While working on this class I found gitpython library which could be used in place of this class.

--- Repository.py
+++ Repository.py
@@ -4,6 +4,9 @@ A class to encapsulate a version control repository such as Git
 
 @author: manujinda
 '''
+import os
+import re
+
 
 class Repository( object ):
     '''
@@ -15,7 +18,71 @@ class Repository( object ):
         '''
         Constructor
         '''
-        self.uri = repo
+        git_ssh = re.compile( '^(git@)([a-zA-Z0-9]*)\.(org|com):([a-zA-Z0-9_]*)/([a-zA-Z0-9_-]*)\.(git)$' )
+        git_https = re.compile( '^(https://)(?:[a-zA-Z0-9_]*@|)([a-zA-Z0-9_]*).(com|org)/([a-zA-Z0-9_]*)/([a-zA-Z0-9_-]*).(git)$' )
+
+        self.uri = repo.strip()
+
+        valid_rui = git_ssh.match( self.uri )
+
+        if not valid_rui:
+            valid_rui = git_https.match( self.uri )
+
+        if valid_rui:
+            self.protocol = 'https' if valid_rui.group( 1 ) == 'https://' else 'ssh'
+            self.host = valid_rui.group( 2 )
+            self.user_name = valid_rui.group( 4 )
+            self.repo_name = valid_rui.group( 5 )
+            self.repo_type = valid_rui.group( 6 )
+        else:
+            self.uri = ''
+            self.protocol = ''
+            self.host = ''
+            self.user_name = ''
+            self.repo_name = ''
+            self.repo_type = ''
+            print 'Invalid git uri'
+
+#         if valid_rui:
+#             self.repo_type = 'git'
+#             self.protocol = 'ssh'
+#             self.host = valid_rui.group( 2 )
+#             self.user_name = valid_rui.group( 4 )
+#             self.repo_name = valid_rui.group( 5 )
+#         else:
+#             valid_rui = git_https.match( self.uri )
+#
+#             if valid_rui:
+#                 self.repo_type = 'git'
+#                 self.protocol = 'https'
+#                 self.host = valid_rui.group( 3 )
+#                 self.user_name = valid_rui.group( 5 )
+#                 self.repo_name = valid_rui.group( 6 )
+#             else:
+#                 self.repo_type = ''
+#                 self.protocol = ''
+#                 self.host = ''
+#                 self.user_name = ''
+#                 self.repo_name = ''
+#                 print 'Invalid git uri'
+
 
     def __str__( self ):
-        return self.uri
+        desc = ''
+        for f in sorted( self.__dict__.keys() ):
+            desc += '{} > {} \n'.format( f, self.__dict__[f] )
+        return desc
+
+    def clone( self, path ):
+        '''
+        clone the git repo at the location provided.
+        Checks whether already cloned beforehand.
+        '''
+        try:
+            os.chdir( path )
+        except OSError:
+            print 'Unable to go to directory {} for cloning'.format( path )
+
+        if not os.path.join( path, self.repo_name ):
+            if self.protocol == 'git':
+                cmd = 'git clone' + self.uri
