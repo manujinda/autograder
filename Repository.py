776aab7fc22122826c2b961de2747572b12862b5
Manujinda Wathugala <manu@cs.uoregon.edu>
2016-08-30 18:35:53 -0700

    Improved the regular expression for git uri
    Improved the user name part of the regular expression for the git uri

--- Repository.py
+++ Repository.py
@@ -22,8 +22,8 @@ class Repository( object ):
         '''
         # git_ssh = re.compile( '^(git@)([a-zA-Z0-9]+)\.(org|com):([a-zA-Z0-9_]+)/([a-zA-Z0-9_-]+)\.(git)$' )
         # git_https = re.compile( '^(https://)(?:[a-zA-Z0-9_]+@|)([a-zA-Z0-9_]+).(com|org)/([a-zA-Z0-9_]+)/([a-zA-Z0-9_-]+).(git)$' )
-        git_ssh = re.compile( '^(git@)([a-zA-Z0-9]+)\.(?:org|com):([a-zA-Z0-9_]+)/([a-zA-Z0-9_-]+)\.(git)$' )
-        git_https = re.compile( '^(https://)(?:[a-zA-Z0-9_]+@|)([a-zA-Z0-9_]+).(?:com|org)/([a-zA-Z0-9_]+)/([a-zA-Z0-9_-]+).(git)$' )
+        git_ssh = re.compile( '^(git@)([a-zA-Z0-9]+)\.(?:org|com):([a-zA-Z0-9]+-{0,1}[a-zA-Z0-9]+)/([a-zA-Z0-9_-]+)\.(git)$' )
+        git_https = re.compile( '^(https://)(?:[a-zA-Z0-9_]+@|)([a-zA-Z0-9_]+).(?:com|org)/([a-zA-Z0-9]+-{0,1}[a-zA-Z0-9]+)/([a-zA-Z0-9_-]+).(git)$' )
 
         self.uri = repo.strip()
 
